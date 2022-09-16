1981b08fe700c1dff864a867dc48eff8d1235212
adison <adison.soares7@gmail.com>
2022-09-16 15:01:14 -0300

    Java orientado a objetos

---
+++ src/Organizacao/CriaConta.java
@@ -0,0 +1,35 @@
+package Organizacao;
+
+public class CriaConta {
+
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println(primeiraConta.saldo);
+		
+		primeiraConta.saldo += 100;
+		System.out.println(primeiraConta.saldo);
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 300;
+		System.out.println("Saldo primeira conta:"+primeiraConta.saldo);
+		System.out.println("Saldo segunda conta:"+segundaConta.saldo);
+		
+		primeiraConta.agencia = 146;
+		System.out.println("Agencia da primeira conta: "+primeiraConta.agencia);
+		
+		System.out.println(primeiraConta.agencia);
+		System.out.println(primeiraConta.numero);
+		
+		segundaConta.agencia = 146;
+		System.out.println("Agencia da segunda conta: "+segundaConta.agencia);
+		
+		if (primeiraConta == segundaConta) {
+			System.out.println("São a mesma conta!");
+		}else {
+			System.out.println("Contas diferentes!");
+		}
+
+	}
+
+}
---
+++ src/Organizacao/TesteMetodo.java
@@ -0,0 +1,29 @@
+package Organizacao;
+
+public class TesteMetodo {
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		boolean retirada = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
+		System.out.println("Retirada: "+retirada);
+		
+		Conta contaDaMarcela = new Conta();
+		
+		contaDaMarcela.deposita(1000);
+		if(contaDaMarcela.transfere(300, contaDoPaulo)) {
+			System.out.println("Transferência realizada!");
+		}else {
+			System.out.println("Transferência não realizada!");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
+		
+		contaDoPaulo.titular = "Paulo Silveira";
+		System.out.println("Titular: "+contaDoPaulo.titular);
+		
+	}
+}
